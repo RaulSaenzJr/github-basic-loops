c8ad3623ad561632d862d855ec33b629974b9c27
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 14:26:29 -0500

    changed for loop to while loop

--- problems/02-while-loop-translation.js
+++ problems/02-while-loop-translation.js
@@ -6,21 +6,24 @@ to use a `while` loop instead of a `for` loop!
 */
 
 function eCounter(word) {
+  // move index = 0 outside of the for loop to change to while loop
+  let index = 0;
   let count = 0;
 
-  for (let index = 0; index < word.length; index++) {
+  while (index < word.length) {
     let char = word[index];
     if (char === "e" || char === "E") {
       count += 1;
     }
+    index += 1;
   }
 
   return count;
 };
 
-// console.log(eCounter("apple"));      // => 1
-// console.log(eCounter("appleapple")); // => 2
-// console.log(eCounter("Appleee"));    // => 3
+console.log(eCounter("apple"));      // => 1
+console.log(eCounter("appleapple")); // => 2
+console.log(eCounter("Appleee"));    // => 3
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
