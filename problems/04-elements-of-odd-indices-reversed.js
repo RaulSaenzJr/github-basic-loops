b10879f1b31efe1cebc74f30dd3fe3686612aea9
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 14:50:18 -0500

    implemented elementsOfOddIndicesReversed function

--- problems/04-elements-of-odd-indices-reversed.js
+++ problems/04-elements-of-odd-indices-reversed.js
@@ -6,12 +6,18 @@ string in reverse order.
 
 function elementsOfOddIndicesReversed(str) {
   // Your code here 
+  let reversed = '';
+  for (let index = 1; index < str.length; index += 2) {
+    reversed = str[index] + reversed;
+  }
+  return reversed;
 }
 
-// console.log(elementsOfOddIndicesReversed('academy')); // 'mdc'
-// console.log(elementsOfOddIndicesReversed('planet'));  // 'tnl'
-// console.log(elementsOfOddIndicesReversed('sport'));   // 'rp'
-
+console.log(elementsOfOddIndicesReversed('academy')); // 'mdc'
+console.log(elementsOfOddIndicesReversed('planet'));  // 'tnl'
+console.log(elementsOfOddIndicesReversed('sport'));   // 'rp'
+console.log(elementsOfOddIndicesReversed('Aurora'));  // 'aou 
+console.log(elementsOfOddIndicesReversed('hello'));   // 'le'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = elementsOfOddIndicesReversed;
