40f13dd4dba6099c6f9e737aec428e363c673774
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 16:20:02 -0500

    created countVowels function

--- problems/09-vowel-counter.js
+++ problems/09-vowel-counter.js
@@ -6,11 +6,29 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+// create a function called countVowels that takes in a word
+function countVowels(word) {
+    // use const because the vowels will not change
+    // create a variable to store the vowels
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    // create a variable to store the count
+    let count = 0;
+    // create a for loop to iterate through the word
+    for (let char of word) {
+        // check if the word includes the vowels
+        if (vowels.includes(char)) {
+            // increment the count
+            count++;
+        }
+    }
+    return count;
+   
+}
 
-// console.log(countVowels("bootcamp")); // => 3
-// console.log(countVowels("apple"));    // => 2
-// console.log(countVowels("pizza"));    // => 2
-// console.log(countVowels("try"));      // => 0
+console.log(countVowels("bootcamp")); // => 3
+console.log(countVowels("apple"));    // => 2
+console.log(countVowels("pizza"));    // => 2
+console.log(countVowels("try"));      // => 0
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = countVowels;
