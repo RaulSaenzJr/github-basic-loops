557f788a3945e24b44b8b1fbe75e1b052dfe277f
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 14:14:57 -0500

    changed while loop to for loop

--- problems/01-for-loop-translation.js
+++ problems/01-for-loop-translation.js
@@ -5,22 +5,27 @@ lowercase (a) and uppercase (A). Your job is to translate the following function
 to use a `for` loop instead of the `while` loop it is currently using.
 */
 
+/**
+ * Counts the number of occurrences of the letter 'a' (both lowercase and uppercase) in a given word.
+ * The function uses a for loop to iterate over each character in the word.
+ * The loop initializes an index variable to 0, checks if the index is less than the length of the word,
+ * and increments the index by 1 after each iteration.
+ * Inside the loop, it checks if the current character is 'a' or 'A' and increments the count accordingly.
+ */
 function aCounter(word) {
-  let index = 0;
   let count = 0;
-  while (index < word.length) {
+  for (let index = 0; index < word.length; index++) {
     let char = word[index];
     if (char === "a" || char === "A") {
       count += 1;
     }
-    index++;
   }
   return count;
-};
+}
 
-// console.log(aCounter("apple"));      // => 1
-// console.log(aCounter("appleapple")); // => 2
-// console.log(aCounter("aAapple"));    // => 3
+console.log(aCounter("apple"));      // => 1
+console.log(aCounter("appleapple")); // => 2
+console.log(aCounter("aAapple"));    // => 3
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
