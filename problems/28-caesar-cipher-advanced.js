8b7e87159c619e43edf9e1d1046b584b008f0785
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 16:06:25 -0500

    caesarCipher

--- problems/28-caesar-cipher-advanced.js
+++ problems/28-caesar-cipher-advanced.js
@@ -8,11 +8,35 @@ Feel free to use this variable: let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 function caesarCipher(string, num) {
     // Your code here 
+    // create a variable to store the alphabet
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    // create a variable to store the new string
+    let newStr = '';
+    // create a for loop to iterate through the string
+    for (let char of string) {
+        // check if the character is a letter
+        if (alphabet.includes(char)) {
+            // add the character to the new string
+            newStr += shiftChar(char, num, alphabet);
+        } else {
+            // add the character to the new string
+            newStr += char;
+        }
+        
+        // Helper function to shift a character by num positions in the alphabet
+        function shiftChar(char, num, alphabet) {
+            let currentIndex = alphabet.indexOf(char);
+            let newIndex = (currentIndex + num) % alphabet.length;
+            return alphabet[newIndex];
+        }
+    }
+    // return the new string
+    return newStr;
 }
 
-// console.log(caesarCipher("apple", 1)); // "bqqmf"
-// console.log(caesarCipher("bootcamp", 2)); // "dqqvecor"
-// console.log(caesarCipher("zebra", 4)); // "difve"
+console.log(caesarCipher("apple", 1)); // "bqqmf"
+console.log(caesarCipher("bootcamp", 2)); // "dqqvecor"
+console.log(caesarCipher("zebra", 4)); // "difve"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = caesarCipher;
