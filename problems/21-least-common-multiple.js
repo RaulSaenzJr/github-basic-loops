21f982da24e4bedfdadfe8be86db85cb06351518
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 15:53:31 -0500

    leastCommonMultiple

--- problems/21-least-common-multiple.js
+++ problems/21-least-common-multiple.js
@@ -5,10 +5,25 @@ both num1 and num2.
 */
 
 // Your code here 
+// Write a function called leastCommonMultiple that accepts two numbers as arguments
+function leastCommonMultiple(num1, num2) {
+    // create a variable to store the max number
+    let max = Math.max(num1, num2);
+    // create a while loop to iterate through the numbers
+    while (true) {
+        // check if the max number is divisible by both numbers
+        if (max % num1 === 0 && max % num2 === 0) {
+            // return the max number
+            return max;
+        }
+        // increment the max number
+        max++;
+    }
+}
 
-// console.log(leastCommonMultiple(4, 6)); // 12
-// console.log(leastCommonMultiple(3, 5)); // 15
-// console.log(leastCommonMultiple(2, 10)); // 10
+console.log(leastCommonMultiple(4, 6)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = leastCommonMultiple;
