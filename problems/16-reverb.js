f79878c8aa7556a2698e3d4d8eb12e2da0897196
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 15:44:43 -0500

    Defined reverb function

--- problems/16-reverb.js
+++ problems/16-reverb.js
@@ -8,13 +8,45 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+// Write reverb function that accepts a word as an argument
+function reverb(word) {
+    // create a variable to store the vowels
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    // create a variable to store the new word
+    let newWord = '';
+    // create a variable to store the last vowel
+    let lastVowel = null;
+    // check if the word is not a string
+    if (typeof word !== 'string') {
+        // return null
+        return null;
+    }
+    // create a for loop to iterate through the word
+    for (let i = 0; i < word.length; i++) {
+        // check if the vowels includes the word
+        if (vowels.includes(word[i].toLowerCase())) {
+            // set the lastVowel to the current vowel
+            lastVowel = i;
+        }
+    }
+    // create a for loop to iterate through the word
+    for (let i = 0; i < word.length; i++) {
+        // check if the index is greater than or equal to the lastVowel
+        if (i >= lastVowel) {
+            // add the character to the new word
+            newWord += word[i];
+        }
+    }
+    // return the new word
+    return word + newWord;
+}
 
-// console.log(reverb('running')); // runninging
-// console.log(reverb('FAMILY'));  // FAMILYILY
-// console.log(reverb('trash'));   // trashash
-// console.log(reverb('DISH'));    // DISHISH
-// console.log(reverb('197393'));  // 197393
-// console.log(reverb(197393));    // null
+console.log(reverb('running')); // runninging
+console.log(reverb('FAMILY'));  // FAMILYILY
+console.log(reverb('trash'));   // trashash
+console.log(reverb('DISH'));    // DISHISH
+console.log(reverb('197393'));  // 197393
+console.log(reverb(197393));    // null
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = reverb;
