1783c5b3d86f04435c84ef7351cab2bde1684e5d
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 16:04:26 -0500

    vowelCipher

--- problems/27-vowel-cipher-advanced.js
+++ problems/27-vowel-cipher-advanced.js
@@ -6,9 +6,39 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+// Write a function called vowelCipher that takes in a string argument
+function vowelCipher(str1) {
+    // create a variable to store the vowels
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    // create a variable to store the vowels
+    let newStr = '';
+    // create a for loop to iterate through the string
+    for (let char of str1) {
+        // check if the string includes the vowels
+        if (vowels.includes(char.toLowerCase())) {
+            // add the character to the new string
+            newStr += nextVowel(char);
+        } else {
+            // add the character to the new string
+            newStr += char;
+        }
+    }
+    // return the new string
+    return newStr;
+}
 
-// console.log(vowelCipher("bootcamp")); // "buutcemp"
-// console.log(vowelCipher("paper cup")); // "pepir cap"
+// Define the nextVowel function
+function nextVowel(char) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let lowerChar = char.toLowerCase();
+    let index = vowels.indexOf(lowerChar);
+    let nextIndex = (index + 1) % vowels.length;
+    let nextChar = vowels[nextIndex];
+    return char === lowerChar ? nextChar : nextChar.toUpperCase();
+}
+
+console.log(vowelCipher("bootcamp")); // "buutcemp"
+console.log(vowelCipher("paper cup")); // "pepir cap"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = vowelCipher;
