86695c5b2607121f98eb7d309d5784c5c841e8b7
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 15:31:37 -0500

    made the abbreviate function

--- problems/13-abbreviate.js
+++ problems/13-abbreviate.js
@@ -6,10 +6,27 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+// Write a function called abbreviateWord that takes in a string argument
+function abbreviate(str1) {
+    // create a variable to store the vowels
+    const vowel = ['a', 'e', 'i', 'o', 'u'];
+    // create a variable to store the new string
+    let newStr = '';
+    // create a for loop to iterate through the string
+    for (let char of str1) {
+        // check if the string includes the vowels
+        if (!vowel.includes(char.toLowerCase())) {
+            // add the character to the new string
+            newStr += char;
+        }
+    }
+    // return the new string
+    return newStr;
+}
 
-// console.log(abbreviate('wonderful')); // 'wndrfl'
-// console.log(abbreviate('mystery')); // 'mystry'
-// console.log(abbreviate('Accordian')); // 'ccrdn'
+console.log(abbreviate('wonderful')); // 'wndrfl'
+console.log(abbreviate('mystery')); // 'mystry'
+console.log(abbreviate('Accordian')); // 'ccrdn'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = abbreviate;
