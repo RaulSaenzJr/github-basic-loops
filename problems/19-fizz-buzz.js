4b100fb6afe8a202846abb7007231eeef3f9c3f2
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 15:50:01 -0500

    created fizzBuzz function

--- problems/19-fizz-buzz.js
+++ problems/19-fizz-buzz.js
@@ -3,9 +3,21 @@ Define a function fizzBuzz(max) that takes a number and prints every number from
 0 to max (not inclusive) that is divisible by either 3 or 5, but not both.
 */
 
-// Your code here 
+// Your code here
+// define a function called fizzBuzz that takes in a number
+// and prints every number from 0 to max (not inclusive) that is divisible by either 3 or 5, but not both
+function fizzBuzz(max) {
+    // create a for loop to iterate through the numbers
+    for (let i = 0; i < max; i++) {
+        // check if the number is divisible by 3 or 5 but not both
+        if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
+            // print out the number
+            console.log(i);
+        }
+    }
+} 
 
-// fizzBuzz(20); // prints out:
+fizzBuzz(20); // prints out:
 /*
 3
 5
