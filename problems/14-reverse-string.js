e7ff6acc7ba895745d156d79d0dba2816e0b411f
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 15:33:30 -0500

    created a reverseString function

--- problems/14-reverse-string.js
+++ problems/14-reverse-string.js
@@ -4,9 +4,21 @@ return a new string where the order of the characters is reversed.
 */
 
 // Your code here 
+// Write a function called reverseString that takes in a string argument
+function reverseString(str1) {
+    // create a variable to store the new string
+    let newStr = '';
+    // create a for loop to iterate through the string
+    for (let i = str1.length - 1; i >= 0; i--) {
+        // add the character to the new string
+        newStr += str1[i];
+    }
+    // return the new string
+    return newStr;
+}
 
-// console.log(reverseString('fish')); // 'hsif'
-// console.log(reverseString('marathon')); // 'nohtaram'
+console.log(reverseString('fish')); // 'hsif'
+console.log(reverseString('marathon')); // 'nohtaram'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = reverseString;
