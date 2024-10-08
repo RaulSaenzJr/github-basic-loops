19799befc24fb3130410b79ceedb3ed4e9953958
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 15:59:41 -0500

    created tripletTrue

--- problems/25-triplet-true.js
+++ problems/25-triplet-true.js
@@ -5,12 +5,26 @@ of the same character consecutively.
 */
 
 // Your code here 
+// Write a function called tripletTrue that accepts a string argument
+function tripletTrue(str) {
+    // create a for loop to iterate through the string
+    for (let i = 0; i < str.length; i++) {
+        // check if the current character is equal to the next two characters
+        if (str[i] === str[i + 1] && str[i] === str[i + 2]) {
+            // return true
+            return true;
+        }
+    }
+    // return false
+    return false;
+}
 
-// console.log(tripletTrue('caaabb'));        // true
-// console.log(tripletTrue('terrrrrible'));   // true
-// console.log(tripletTrue('runninggg'));     // true
-// console.log(tripletTrue('bootcamp'));      // false
-// console.log(tripletTrue('e'));             // false
+
+console.log(tripletTrue('caaabb'));        // true
+console.log(tripletTrue('terrrrrible'));   // true
+console.log(tripletTrue('runninggg'));     // true
+console.log(tripletTrue('bootcamp'));      // false
+console.log(tripletTrue('e'));             // false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = tripletTrue;
