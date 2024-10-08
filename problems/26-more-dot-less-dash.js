752d8970ce5b6700530c24ab7f1292e1b7da3b7f
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-08 16:01:18 -0500

    moreDotLessDash

--- problems/26-more-dot-less-dash.js
+++ problems/26-more-dot-less-dash.js
@@ -5,12 +5,39 @@ more dots (.) than dashes (-).
 */
 
 // Your code here 
+// Write a function called moreDotLessDash that accepts a string argument
+function moreDotLessDash(str) {
+    // create a variable to store the number of dots
+    let dotCount = 0;
+    // create a variable to store the number of dashes
+    let dashCount = 0;
+    // create a for loop to iterate through the string
+    for (let i = 0; i < str.length; i++) {
+        // check if the current character is a dot
+        if (str[i] === '.') {
+            // increment the dot count
+            dotCount++;
+        }
+        // check if the current character is a dash
+        if (str[i] === '-') {
+            // increment the dash count
+            dashCount++;
+        }
+    }
+    // check if the dot count is greater than the dash count
+    if (dotCount > dashCount) {
+        // return true
+        return true;
+    }
+    // return false
+    return false;
+}
 
-// console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
-// console.log(moreDotLessDash('Morse code is great.'));                   // true
-// console.log(moreDotLessDash('.... . -.--'));                            // true
-// console.log(moreDotLessDash('.--. .-. --- --. .-. .- -- -- . .-.'));    // false
-// console.log(moreDotLessDash('high-flying acrobat.'));                   // false
+console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
+console.log(moreDotLessDash('Morse code is great.'));                   // true
+console.log(moreDotLessDash('.... . -.--'));                            // true
+console.log(moreDotLessDash('.--. .-. --- --. .-. .- -- -- . .-.'));    // false
+console.log(moreDotLessDash('high-flying acrobat.'));                   // false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = moreDotLessDash;
